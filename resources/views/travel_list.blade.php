eba293473840c1499006df789a40a273d18f2c02
unknown <jaka_prog@mpotech.net>
2021-03-04 17:47:58 +0700

    pongg

--- resources/views/travel_list.blade.php
+++ resources/views/travel_list.blade.php
@@ -19,7 +19,7 @@
                     <div class="column x5">
 
                         <ul class="list">
-                            <li class="header togo">abc</li>
+                            <li class="header togo">abcd</li>
                             @foreach ($togo as $newplace)
                                 <li>{{ $newplace->name }}</li>
                             @endforeach
