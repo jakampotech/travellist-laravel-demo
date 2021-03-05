6684520d9f1550345489b39198ffa265e85d0a40
unknown <jaka_prog@mpotech.net>
2021-03-05 17:30:39 +0700

    negara - city

--- resources/views/travel_list.blade.php
+++ resources/views/travel_list.blade.php
@@ -19,7 +19,7 @@
                     <div class="column x5">
 
                         <ul class="list">
-                            <li class="header togo">Negara</li>
+                            <li class="header togo">City</li>
                             @foreach ($togo as $newplace)
                                 <li>{{ $newplace->name }}</li>
                             @endforeach
